c32dd3f9c118557879ea097a3315bd36a0b90969
ycruel <245614714@qq.com>
2021-12-04 18:08:40 +0800

    2

--- src/main/java/com/gittest/Gittest.java
+++ src/main/java/com/gittest/Gittest.java
@@ -15,5 +15,6 @@ package com.gittest;/**
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
