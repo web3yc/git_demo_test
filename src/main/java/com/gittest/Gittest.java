790bf5d4a7ac08e12c9f0ee6eb0017e1b8eb04dd
ycruel <245614714@qq.com>
2021-12-05 09:54:15 +0800

    hot-fix 1

--- src/main/java/com/gittest/Gittest.java
+++ src/main/java/com/gittest/Gittest.java
@@ -16,5 +16,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello hot-fix");
     }
 }
